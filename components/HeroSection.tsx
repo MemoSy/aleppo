830ee2db3001611ed4f61fadd6fd3f20067dba34
MemoSy <ahmeddalhalabi1@gmail.com>
2024-12-07 19:40:43 +0300

    second push

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import ImmediateNewsBar from "./ImmediateNewsBar";
+import Image from "next/image";
 
 
 export function HeroSection() {
@@ -17,21 +18,24 @@ export function HeroSection() {
   }, []);
 
   return (
-    <div className="relative w-full flex justify-center items-center mb-[73px] z-10 bg-[url('/bgforhero.jpg')] bg-cover bg-[50%_50%] md:bg-top py-[390px]">
+    <div className="relative w-full flex justify-center items-center mb-[73px] z-10 md:bg-top py-[390px]">
       {/* Cloud background overlay */}
+      <Image
+        src="/bgforhero.jpg"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        className="bg-top"
+      />
       <div
         className="absolute z-20 w-full h-full flex justify-center items-center"
-        style={{
-          backgroundImage: "url('')",
-          backgroundSize: "cover",
-        }}
       >
         {/* Hero content */}
-        <div className="text-center relative z-50 -translate-y-24">
+        <div className="text-center relative z-50 -translate-y-64 -translate-x-8">
           <h1 className="text-4xl md:text-6xl font-bold text-gray-50 mb-4">
             صوت حلب
           </h1>
-          <p className="text-lg md:text-xl text-gray-600">
+          <p className="text-lg md:text-xl text-gray-300">
           أنت تتكلم ... و المدينة تستجيب
           </p>
         </div>
